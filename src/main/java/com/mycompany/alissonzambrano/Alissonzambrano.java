723380152cf29a8b7e8ba6594ca6f241b207f18e
zamdayana <alissonzambrano88@gmail.com>
2024-11-26 11:25:11 -0500

    Prueba

---
+++ src/main/java/com/mycompany/alissonzambrano/Alissonzambrano.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.alissonzambrano;
+
+/**
+ *
+ * @author aliss
+ */
+public class Alissonzambrano {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
